fbb065c6d48afdfdd0ee1c93985f62f813e05969
Koochr <dm.girenko@gmail.com>
2021-06-21 22:02:33 +0300

    Add nft function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -45,3 +45,64 @@ export const auth = https.onRequest((req, res) =>
 		}
 	})
 )
+
+export const addDaoNft = https.onRequest((req, res) =>
+	cors()(req, res, async () => {
+		try {
+			if (req.method !== "POST") {
+				res.status(400).end("Only POST method is supported")
+				return
+			}
+
+			if (!(req.headers.authorization && req.headers.authorization.startsWith("Bearer "))) {
+				res.status(401).send("Unauthorized")
+				return
+			}
+			let user: string
+			const idToken = req.headers.authorization.split("Bearer ")[1]
+			try {
+				user = (await admin.auth().verifyIdToken(idToken)).uid
+			} catch (error) {
+				res.status(401).send("Unauthorized")
+				return
+			}
+
+			if (!(req.body?.address && req.body.nft)) {
+				res.status(400).end("Bad Payload")
+				return
+			}
+
+			const {address, nft} = req.body
+
+			const dao = await admin.firestore().collection("DAOs").doc(address).get()
+			if (!dao.exists) {
+				res.status(400).end("DAO not found")
+				return
+			}
+
+			const member = await admin
+				.firestore()
+				.collection("daoUsers")
+				.where("dao", "==", address)
+				.where("address", "==", user)
+				.get()
+			if (member.empty) {
+				res.status(403).send("Forbidden")
+				return
+			}
+
+			await admin
+				.firestore()
+				.collection("nfts")
+				.add({
+					...nft,
+					nftAdminUserUID: address
+				})
+
+			res.status(200).end("OK")
+		} catch (e) {
+			console.error(e)
+			res.sendStatus(500)
+		}
+	})
+)
