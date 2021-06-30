786594dad517f0323cfd28be1ca97189b33fa924
Koochr <dm.girenko@gmail.com>
2021-06-30 20:35:19 +0300

    Implement edit dao function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -111,3 +111,66 @@ export const addDaoNft = https.onRequest((req, res) =>
 		}
 	})
 )
+
+export const editDao = https.onRequest((req, res) =>
+	cors()(req, res, async () => {
+		try {
+			if (req.method !== "POST") {
+				res.status(400).end("Only POST method is supported")
+				return
+			}
+
+			if (!(req.headers.authorization && req.headers.authorization.startsWith("Bearer "))) {
+				res.status(401).send("Unauthorized")
+				return
+			}
+			let user: string
+			const idToken = req.headers.authorization.split("Bearer ")[1]
+			try {
+				user = (await admin.auth().verifyIdToken(idToken)).uid
+			} catch (error) {
+				res.status(401).send("Unauthorized")
+				return
+			}
+
+			if (!req.body?.address) {
+				res.status(400).end("Bad Payload")
+				return
+			}
+
+			const {address, name, description, website, twitter, telegram, discord} = req.body
+
+			const dao = await admin.firestore().collection("DAOs").doc(address).get()
+			if (!dao.exists) {
+				res.status(400).end("DAO not found")
+				return
+			}
+
+			const member = await admin
+				.firestore()
+				.collection("daoUsers")
+				.where("dao", "==", address)
+				.where("address", "==", user)
+				.where("role", "in", ["head", "admin"])
+				.get()
+			if (member.empty) {
+				res.status(403).send("Forbidden")
+				return
+			}
+
+			await admin.firestore().collection("DAOs").doc(address).update({
+				name,
+				description,
+				website,
+				twitter,
+				telegram,
+				discord
+			})
+
+			res.status(200).end("OK")
+		} catch (e) {
+			console.error(e)
+			res.sendStatus(500)
+		}
+	})
+)
