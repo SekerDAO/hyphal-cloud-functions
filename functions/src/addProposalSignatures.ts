cb8cf0f80f24ca441bc5ba107ee97fe59e65b3ff
Koochr <dm.girenko@gmail.com>
2021-08-06 06:08:40 +0300

    Implement add proposal signature endpoint

---
+++ functions/src/addProposalSignatures.ts
@@ -0,0 +1,70 @@
+import {https} from "firebase-functions"
+import cors from "cors"
+import admin from "firebase-admin"
+
+const addProposalSignatures = https.onRequest((req, res) =>
+	cors()(req, res, async () => {
+		try {
+			if (req.method !== "POST") {
+				res.status(400).end("Only POST method is supported")
+				return
+			}
+
+			if (!(req.headers.authorization && req.headers.authorization.startsWith("Bearer "))) {
+				res.status(401).send("Unauthorized")
+				return
+			}
+			let user: string
+			const idToken = req.headers.authorization.split("Bearer ")[1]
+			try {
+				user = (await admin.auth().verifyIdToken(idToken)).uid
+			} catch (error) {
+				res.status(401).send("Unauthorized")
+				return
+			}
+
+			if (!(req.body?.proposalId && req.body.signature)) {
+				res.status(400).end("Bad Payload")
+				return
+			}
+
+			const {proposalId, signature, signatureStep2, newState} = req.body
+
+			const proposal = await admin.firestore().collection("proposals").doc(proposalId).get()
+			if (!proposal.exists) {
+				res.status(400).end("DAO not found")
+				return
+			}
+			const {gnosisAddress, signatures, signaturesStep2} = proposal.data()!
+
+			const member = await admin
+				.firestore()
+				.collection("daoUsers")
+				.where("dao", "==", gnosisAddress)
+				.where("address", "==", user)
+				.where("role", "in", ["admin", "head"])
+				.get()
+			if (member.empty) {
+				res.status(403).send("Forbidden")
+				return
+			}
+
+			await admin
+				.firestore()
+				.collection("proposals")
+				.doc(proposalId)
+				.update({
+					signatures: [...signatures, signature],
+					...(signatureStep2 ? {signaturesStep2: [...signaturesStep2, signatureStep2]} : {}),
+					...(newState ? {state: newState} : {})
+				})
+
+			res.status(200).end("OK")
+		} catch (e) {
+			console.error(e)
+			res.sendStatus(500)
+		}
+	})
+)
+
+export default addProposalSignatures
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,9 +4,10 @@ import editDao from "./editDao"
 import addDaoNft from "./addDaoNft"
 import updateDaoUser from "./updateDaoUser"
 import addZoraAuction from "./addZoraAuction"
+import addProposalSignatures from "./addProposalSignatures"
 
 admin.initializeApp({
 	serviceAccountId: "token-walk@appspot.gserviceaccount.com"
 })
 
-export {auth, editDao, addDaoNft, updateDaoUser, addZoraAuction}
+export {auth, editDao, addDaoNft, updateDaoUser, addZoraAuction, addProposalSignatures}
