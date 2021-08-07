927b245661aa6522ca8d0fc09ab74f5807d0e1c0
Koochr <dm.girenko@gmail.com>
2021-08-07 20:56:33 +0300

    Update updateDaoUser function to handle gnosis module

--- functions/src/updateDaoUser.ts
+++ functions/src/updateDaoUser.ts
@@ -2,9 +2,11 @@ import {config, https} from "firebase-functions"
 import cors from "cors"
 import {Contract} from "@ethersproject/contracts"
 import HouseTokenDAO from "./abis/HouseTokenDAO.json"
+import GnosisSafe from "./abis/GnosisSafeL2.json"
 import admin from "firebase-admin"
 import {InfuraProvider} from "@ethersproject/providers"
 import {isAddress} from "@ethersproject/address"
+import {DAOMemberRole} from "../../../TokenWalk/src/types/DAO"
 
 const provider = new InfuraProvider(config().infura.network, {
 	projectId: config().infura.id,
@@ -19,11 +21,11 @@ const updateDaoUser = https.onRequest((req, res) =>
 				return
 			}
 
-			if (!(req.body?.daoAddress && req.body.memberAddress)) {
+			if (!(req.body?.gnosisAddress && req.body.memberAddress)) {
 				res.status(400).end("Bad Payload")
 				return
 			}
-			if (!isAddress(req.body.daoAddress)) {
+			if (!isAddress(req.body.gnosisAddress)) {
 				res.status(400).end("Bad DAO Address")
 				return
 			}
@@ -32,30 +34,34 @@ const updateDaoUser = https.onRequest((req, res) =>
 				return
 			}
 
-			const {daoAddress, memberAddress} = req.body
+			const {gnosisAddress, memberAddress} = req.body
 
-			const dao = await admin.firestore().collection("DAOs").doc(daoAddress).get()
+			const dao = await admin.firestore().collection("DAOs").doc(gnosisAddress).get()
 			if (!dao.exists) {
 				res.status(400).end("DAO not found")
 				return
 			}
-			const {type} = dao.data()!
+			const {type, daoAddress} = dao.data()!
 
-			const daoContract = new Contract(daoAddress, HouseTokenDAO.abi, provider)
-			const member = await daoContract.members(memberAddress)
-			const role = member
-				? member.roles.headOfHouse
-					? type === "house"
-						? "head"
-						: "admin"
-					: member.roles.member
-					? "member"
-					: null
-				: null
+			let role: DAOMemberRole | null = null
+			const safeContract = new Contract(gnosisAddress, GnosisSafe.abi, provider)
+			const isAdmin: boolean = await safeContract.isOwner(memberAddress)
+			if (isAdmin) {
+				role = type === "house" ? "head" : "admin"
+			} else if (daoAddress) {
+				const daoContract = new Contract(daoAddress, HouseTokenDAO.abi, provider)
+				const member = await daoContract.members(memberAddress)
+				if (member?.roles.headOfHouse || member.roles.member) {
+					// TODO: are we planning to add admins of DAO module?
+					role = "member"
+				}
+			}
+
 			const userRoleSnapshot = await admin
 				.firestore()
 				.collection("daoUsers")
 				.where("address", "==", memberAddress)
+				.where("dao", "==", gnosisAddress)
 				.get()
 			const oldRole = userRoleSnapshot.empty ? null : userRoleSnapshot.docs[0].data().role
 
